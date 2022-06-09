9268364ac1d779f4298b78cce43fd83ffe274e92
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-09 15:48:42 +0300

    Response model

---
+++ Defender.Services.Client/Response.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace scan_util;
+
+public class Response<T>
+{
+    [JsonProperty("success")]
+    public bool Success { get; set; }
+    [JsonProperty("errors")]
+    public List<string> Errors { get; set; } = new();
+    [JsonProperty("data")]
+    public T Data { get; set; }
+}

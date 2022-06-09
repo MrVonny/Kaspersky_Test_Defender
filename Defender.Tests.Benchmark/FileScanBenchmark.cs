308ab2505cef88f75070778ef384f45988ed95cf
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-10 01:16:34 +0300

    new columns

--- Defender.Tests.Benchmark/FileScanBenchmark.cs
+++ Defender.Tests.Benchmark/FileScanBenchmark.cs
@@ -22,28 +22,30 @@ public class FileScanBenchmark
     {
         public Config()
         {
-            AddJob(new Job("Base")
-            {
-                Run = { IterationCount = 5}
-            });
-            AddColumn(new FileScannerColumn());
+            SummaryStyle = SummaryStyle.Default.WithMaxParameterColumnWidth(50)
+                .WithRatioStyle(RatioStyle.Trend);
+            //AddColumn(new FileScannerColumn());
+            AddColumn(new ScanSizeColumn());
         }
     }
     
-    public IEnumerable<IFileScanner> Scanners => new FileScanner[] { /*new NaiveScanner(),*/ new AhoCorasickScanner() };
-    public IEnumerable<string> Paths => new[] { /*@"D:\repo\Defender",*/ @"N:\Program Files (x86)\Steam\steamapps\common\dota 2 beta" };
+    public IEnumerable<IFileScanner> Scanners => new FileScanner[] { new NaiveScanner(), new AhoCorasickScanner(), new RegexScanner() };
+    public IEnumerable<string> Paths => new[] { /*@"N:\Program Files (x86)\Steam\steamapps\common\dota 2 beta",*/ @"D:\repo\Defender\Defender.Domain.Core", @"D:\repo\Defender", @"D:\repo"};
+    
+    [ParamsSource(nameof(Paths))]
+    public string Path;
 
     [ParamsSource(nameof(Scanners))]
     public IFileScanner FileScanner;
     
-    [ParamsSource(nameof(Paths))]
-    public string Path;
+    
 
     public FileScanBenchmark()
     {
         _mock.Setup(x => x.SaveChangesAsync()).Returns(Task.FromResult(1));
     }
     
+    [Benchmark]
     public void AllFileScanner()
     {
         var defenderEngine = new DefenderEngine(_mock.Object, FileScanner);
@@ -83,10 +85,46 @@ public class FileScannerColumn : IColumn
 
     public string Id { get; }
     public string ColumnName => "FileScanner";
-    public bool AlwaysShow { get; }
-    public ColumnCategory Category { get; }
+    public bool AlwaysShow => true;
+    public ColumnCategory Category => ColumnCategory.Custom;
     public int PriorityInCategory { get; }
     public bool IsNumeric { get; }
-    public UnitType UnitType { get; }
+    public UnitType UnitType => UnitType.Dimensionless;
+    public string Legend { get; }
+}
+
+public class ScanSizeColumn : IColumn
+{
+    public string GetValue(Summary summary, BenchmarkCase benchmarkCase)
+    {
+        var path = benchmarkCase.Parameters.Items.Single(x => x.Name == nameof(FileScanBenchmark.Path)).Value as string;
+        var fullPath = Path.GetFullPath(path);
+        var files = Directory.GetFiles(fullPath, "*.*", SearchOption.AllDirectories);
+        var size = files.Sum(x => new FileInfo(x).Length);
+        return $"{files.Length} ({(size / 1024.0 / 1024.0).ToString("F")} MB)";
+    }
+
+    public string GetValue(Summary summary, BenchmarkCase benchmarkCase, SummaryStyle style)
+    {
+        return GetValue(summary, benchmarkCase);
+    }
+
+    public bool IsDefault(Summary summary, BenchmarkCase benchmarkCase)
+    {
+        return false;
+    }
+
+    public bool IsAvailable(Summary summary)
+    {
+        return true;
+    }
+
+    public string Id { get; }
+    public string ColumnName => "Files";
+    public bool AlwaysShow => true;
+    public ColumnCategory Category => ColumnCategory.Custom;
+    public int PriorityInCategory { get; }
+    public bool IsNumeric { get; }
+    public UnitType UnitType => UnitType.Dimensionless;
     public string Legend { get; }
 }

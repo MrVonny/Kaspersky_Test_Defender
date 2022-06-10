0355c8e90509c6c4a3d3d3d9b2600106d912d6f9
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-10 14:40:40 +0300

    Unit Tests Initial

---
+++ Defender.Tests.Unit/FakeDefenderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Defender.Domain.Core.Models;
+using Defender.Domain.Interfaces;
+
+namespace Defender.Tests.Unit;
+
+public class FakeDefenderRepository : IDefenderTaskRepository
+{
+    private List<DefenderTask> _defenderTasks = new();
+    private int _counter = 0;
+
+    public void Dispose()
+    {
+        _defenderTasks = new List<DefenderTask>();
+        _counter = 0;
+    }
+
+    public void Add(DefenderTask obj)
+    {
+        obj.Id = ++_counter;
+        _defenderTasks.Add(obj);
+    }
+
+    public Task<DefenderTask> GetById(int id)
+    {
+        return Task.FromResult(_defenderTasks.Single(x => x.Id == id));
+    }
+
+    public void Update(DefenderTask obj)
+    {
+        Remove(obj.Id);
+        _defenderTasks.Add(obj);
+    }
+
+    public void Remove(int id)
+    {
+        _defenderTasks.RemoveAt(_defenderTasks.FindIndex(x=>x.Id == id));
+    }
+
+    public int SaveChanges()
+    {
+        return 0;
+    }
+
+    public Task<int> SaveChangesAsync()
+    {
+        return Task.FromResult(0);
+    }
+}
---
+++ Defender.Tests.Unit/FileScannerTests.cs
@@ -0,0 +1,70 @@
+﻿using Defender.Domain.DefenderEngine;
+using Defender.Domain.DefenderEngine.Scanners;
+using Defender.Domain.Interfaces;
+using Moq;
+
+namespace Defender.Tests.Unit;
+
+public abstract class FileScannerTests
+{
+    protected string TestDataPath => Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "TestData");
+    protected DefenderEngine DefenderEngine;
+    protected IDefenderTaskRepository TaskRepository;
+
+    public FileScannerTests(FileScanner fileScanner)
+    {
+        TaskRepository = new FakeDefenderRepository();
+        DefenderEngine = new DefenderEngine(TaskRepository, fileScanner);
+    }
+
+    [SetUp]
+    public virtual void SetUp()
+    {
+        TaskRepository.Dispose();
+    }
+    
+    public virtual void CreateTestData(int js, int rmrf, int dll, int clean)
+    {
+        new FileGenerator.TestDataGenerator(js, rmrf, dll, clean, TestDataPath).Generate();
+    }
+
+    [Test]
+    [TestCase(0,0,0,0)]
+    [TestCase(0,0,0,10)]
+    [TestCase(10,10,10,0)]
+    [TestCase(12,4,34,10)]
+    public async Task Test(int js, int rmrf, int dll, int clean)
+    {
+        //SetUp
+        CreateTestData(js,rmrf,dll,clean);
+        var task = DefenderEngine.Create(TestDataPath);
+        await DefenderEngine.StartByLines(task);
+
+        task = await TaskRepository.GetById(task.Id);
+        Assert.That(task.JsDetected, Is.EqualTo(js));
+        Assert.That(task.RmRfDetected, Is.EqualTo(rmrf));
+        Assert.That(task.RunDllDetected, Is.EqualTo(dll));
+        Assert.That(task.FilesProcessed, Is.EqualTo(js+rmrf+dll+clean));
+    }
+}
+
+public class NaiveScannerTests : FileScannerTests
+{
+    public NaiveScannerTests() : base(new NaiveScanner())
+    {
+    }
+}
+
+public class RegexScannerTests : FileScannerTests
+{
+    public RegexScannerTests() : base(new RegexScanner())
+    {
+    }
+}
+
+public class AhoCorasickTests : FileScannerTests
+{
+    public AhoCorasickTests() : base(new AhoCorasickScanner())
+    {
+    }
+}
